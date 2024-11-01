25feee8ca99326da85a1aa6fbfa7040c82fed2ab
sviridov6012 <160075897+sviridov6012@users.noreply.github.com>
2024-11-01 18:56:17 +0300

    task2_updt
    я исправил ваше замечание, но в PyCharm в программе курса пишет, что мой ответ неправильный. хотя я согласен с вашим утверждением

--- Лаба 2/task_2.py
+++ Лаба 2/task_2.py
@@ -1,7 +1,7 @@
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
-increase = 0.03  # Ежемесячный рост цен (5%)
+increase = 0.03  # Ежемесячный рост цен (3%)
 
 # Начинаем с нулевой подушки безопасности
 money_capital = 0
@@ -19,6 +19,7 @@ for month in range(months):
     spend *= (1 + increase)
 
 # Округляем до целого числа
-money_capital = round(money_capital)
+import math
+money_capital = math.ceil(money_capital)
 
 print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов: {money_capital}")
